850fe8525f25323b531a04c172932400904fe615
Misha <mnesterenko851@gmail.com>
2024-10-05 23:53:30 +0300

    лабраб-4

--- my1.py
+++ my1.py
@@ -1,4 +1,17 @@
-def reverse_string(s):
-    return s[::-1]
-s = 'GitHub'
-print(reverse_string(s))
+numbers = []
+for i in range(5):
+    num = float(input(f"Введіть число {i+1}: "))
+    numbers.append(num)
+max_value = max(numbers)
+min_value = min(numbers)
+print(f"Максимальне число: {max_value}")
+print(f"Мінімальне число: {min_value}")
+
+def sum_list(lst):
+    return sum(lst)
+numders = [12, 10, 3, 20, 1]
+print(sum_list(numders))  
+
+my_list = [2, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 8]
+unique_elemets = list(set(my_list))
+print(f"Унікальні елементи списку: {unique_elemets}")
