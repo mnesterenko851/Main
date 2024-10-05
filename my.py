e3b2727f76b8321c99ab7561951ac02e4d4276d6
Misha <mnesterenko851@gmail.com>
2024-10-05 19:34:33 +0300

    лабраб-3

--- my.py
+++ my.py
@@ -1,3 +1,16 @@
 name = input("Введіть ваше ім'я:")
 age = input("Введіть ваш вік:")
 print(f"Привіт, {name}! Тобі {age} років")
+
+def reverse_string(s):
+    return s[::-1]
+s = 'GitHub'
+print(reverse_string(s))
+
+vowels = "аеєиіїоуюяАЕЄИІЇОУЮЯ"
+user_string = input("Введіть строку: ")
+vowel_count = 0
+for char in user_string:
+    if char in vowels:
+        vowel_count += 1
+print(f"Кількість голосних літер у введеній строкі: {vowel_count}")
