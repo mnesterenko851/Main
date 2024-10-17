b603050d795429a4dbe347dabe4446aa4b401201
Misha <mnesterenko851@gmail.com>
2024-10-17 18:32:00 +0300

    лабраб-7

--- my4.py
+++ my4.py
@@ -1,3 +1,27 @@
-my_list = [2, 2, 3, 4, 4, 5, 5, 6, 7, 8, 8, 8]
-unique_elemets = list(set(my_list))
-print(f"Унікальні елементи списку: {unique_elemets}")
+students = {
+    "student1": {"name": "John", "age": "24", "city": "New York"},
+    "student2": {"name": "Alice", "age": "25", "city": "Los Angeles"}
+}
+print(students["student1"]["city"])
+
+
+text = "orange orange orange apple apple banana"
+word_count = {}
+for word in text.split():
+    word_count[word] = word_count.get(word, 0 ) + 1
+print(word_count)
+
+
+books = {
+    "book1": {"title": "1984", "author": "Gorge Orwell", "year": "1949"},
+    "book2": {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960}
+}
+print(books["book1"]["title"])
+
+
+text = "hello world"
+char_count = {}
+for char in text:
+    if char != "":
+        char_count[char] = char_count.get(char, 0) + 1
+print(char_count)
