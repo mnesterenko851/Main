3725e636276a935714a081eef548d3c728d32535
Misha <mnesterenko851@gmail.com>
2024-11-01 11:32:36 +0200

    бібліотека до лб-11 3 завдання

---
+++ mymath.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
